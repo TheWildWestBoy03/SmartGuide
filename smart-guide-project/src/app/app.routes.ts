2cf150b0cd8f37551a528cd0984871b7173f6fee
EugenM03 <geanyzz91@gmail.com>
2025-12-08 17:53:03 +0200

    Implementing Frontend: list of pages, sign-in and log-in forms, saving data from sign-in

--- smart-guide-project/src/app/app.routes.ts
+++ smart-guide-project/src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { RecommendationsList } from './recommendations-list/recommendations-list';
+import { SignInPage } from './sign-in-page/sign-in-page';
+import { LoginPage } from './login-page/login-page';
+import { ItinerariesPage } from './itineraries-page/itineraries-page';
+import { HomePage } from './home-page/home-page';
+import { OptionsPage } from './options-page/options-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomePage,
+        title: 'Home'
+    }, {
+        path: 'recommendations',
+        component: RecommendationsList,
+        title: 'Recommendations'
+    }, {
+        path: 'sign-in',
+        component: SignInPage,
+        title: 'Sign In'
+    }, {
+        path: 'login',
+        component: LoginPage,
+        title: 'Log in'
+    }, {
+        path: 'itineraries',
+        component: ItinerariesPage,
+        title: 'Itineraries'
+    }, {
+        path: 'options',
+        component: OptionsPage,
+        title: 'Options'
+    }
+];
--- smart-guide-project/src/app/app.ts
+++ smart-guide-project/src/app/app.ts
@@ -1,12 +1,32 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, Router, NavigationEnd, RouterOutlet} from '@angular/router';
+import { PageList } from './page-list/page-list';
+import { OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PageList, CommonModule, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('smart-guide-project');
+  showHeader : boolean = false;
+
+  constructor(private router : Router) {
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    ).subscribe(event => this.modifyHeader(event))
+  }
+
+  modifyHeader(location : NavigationEnd) {
+    if (location.url === '/sign-in' || location.url === '/login') {
+      this.showHeader = false;
+    } else {
+      this.showHeader = true;
+    }
+  }
 }
